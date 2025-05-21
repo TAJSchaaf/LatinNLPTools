6141a80e27ad0b0fa37c1e62772f5131799d4b53
TAJSchaaf <157992880+TAJSchaaf@users.noreply.github.com>
2025-05-21 20:10:31 +0200

    Cleaned glosses

--- scripts/clean_glosses.R
+++ scripts/clean_glosses.R
@@ -7,19 +7,23 @@ pacman::p_load(
 )
 
 # data import
-glosses = read.csv("/Users/Thea/Desktop/LatinNLPTools/data/glosses.csv", header=TRUE)
+glosses = read.csv("/Users/Thea/Desktop/LatinNLPTools/data/gold_standard_original/original_glosses.csv", header=TRUE)
 
 head(glosses)
 
-# Select only the glossID and Gloss columns
+# Select only the sample_id, word, and lemma columns
 glosses_cleaned <- glosses %>%
-  select(GlossID, Gloss) %>%
-  # Replace '.i.' with 'idest' in Gloss
-  mutate(Gloss = str_replace_all(Gloss, "\\.i\\.", "idest")) %>%
-  # Remove all punctuation from Gloss
-  mutate(Gloss = str_remove_all(Gloss, "[[:punct:]]"))
+  select(sample_id, word, lemma, pos) %>%
+  # Replace '.i.' with 'idest' in word and lemma
+  mutate(word = str_replace_all(word, "\\.i\\.", "idest")) %>%
+  mutate(word = str_replace_all(word, "i\\.", "idest")) %>%
+  mutate(lemma = str_replace_all(lemma, "\\.i\\.", "idest")) %>%
+  mutate(lemma = str_replace_all(lemma, "i\\.", "idest")) %>%
+  # Replace '???' with '?' in word and lemma
+  mutate(word = str_replace_all(word, fixed("???"), "?")) %>%
+  mutate(lemma = str_replace_all(lemma, fixed("???"), "?"))
 
 head(glosses_cleaned)
 
 # Write the cleaned data to a new CSV file
-write_csv(glosses_cleaned, "/Users/Thea/Desktop/LatinNLPTools/data/glosses_cleaned.csv")
+write_csv(glosses_cleaned, "/Users/Thea/Desktop/LatinNLPTools/data/gold_standard/gs_glosses.csv")
